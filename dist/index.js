aff5d4cfb37f120023d01b28c5bd2dd92f310c1c
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-20 15:43:38 +0500

    added: complied js code

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log("Hello World!");
+let age = 23;
+if (age > 20) {
+    age += 10;
+}
+console.log(age);
+let sales = 237303003;
+let course = "Typescript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [];
+let number = [1, 2, '3'];
+//# sourceMappingURL=index.js.map
