56513fc1e4cff1955c9314814690c82cdd1cad74
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-21 13:24:41 +0500

    javascript complied code

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,54 @@
 "use strict";
-console.log("Hello World!");
-let age = 23;
-if (age > 20) {
-    age += 10;
+var _a;
+const Small = 1;
+const Medium = 2;
+const Large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    else {
+        return income * 1.5;
+    }
 }
-console.log(age);
-let sales = 237303003;
-let course = "Typescript";
-let is_published = true;
-let level;
-function render(document) {
-    console.log(document);
+calculateTax(10000, 2023);
+let employee = {
+    id: 1,
+    name: "Jehangeer",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
 }
-let numbers = [];
-let number = [1, 2, '3'];
+kgToLbs(10);
+kgToLbs("10kg");
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
+}
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
